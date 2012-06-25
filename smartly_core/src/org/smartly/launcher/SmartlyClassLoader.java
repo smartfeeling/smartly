d53197f8a05b6229642937a37cbca0a49fabc79b
Gian Angelo Geminiani <angelo.geminiani@gmail.com>
2012-06-25 11:13:24 +0200

    adjust

--- smartly_core/src/org/smartly/launcher/SmartlyClassLoader.java
+++ smartly_core/src/org/smartly/launcher/SmartlyClassLoader.java
@@ -43,6 +43,7 @@ public class SmartlyClassLoader extends URLClassLoader {
         super(urls, parent);
     }
 
+
     // --------------------------------------------------------------------
     //               p r o t e c t e d
     // --------------------------------------------------------------------
