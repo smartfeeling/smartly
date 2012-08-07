01fae977177a6795caf5b460c5298d68877aeec2
Gian Angelo Geminiani <angelo.geminiani@gmail.com>
2012-08-07 11:59:39 +0200

    Locale: added support for resource lookup. i.e. "propvalue=file_en.html" return content of file.

--- smartly_core/src/org/smartly/commons/i18n/impl/BaseDictionary.java
+++ smartly_core/src/org/smartly/commons/i18n/impl/BaseDictionary.java
@@ -4,7 +4,10 @@
 package org.smartly.commons.i18n.impl;
 
 import org.smartly.commons.i18n.AbstractI18nBundle;
+import org.smartly.commons.util.ClassLoaderUtils;
 import org.smartly.commons.util.LocaleUtils;
+import org.smartly.commons.util.PathUtils;
+import org.smartly.commons.util.StringUtils;
 
 import java.util.Locale;
 import java.util.Map;
@@ -18,8 +21,13 @@ import java.util.Map;
  */
 public class BaseDictionary extends AbstractI18nBundle {
 
+    private boolean _lookupForFileResource = false;
 
-    public BaseDictionary(Class refererClass) {
+    // --------------------------------------------------------------------
+    //               c o n s t r u c t o r
+    // --------------------------------------------------------------------
+
+    public BaseDictionary(final Class refererClass) {
         super(refererClass);
     }
 
@@ -27,17 +35,17 @@ public class BaseDictionary extends AbstractI18nBundle {
         super();
     }
 
-    public String getMessage(final String key, final Locale locale,
-                             final Object... args) {
-        return super.getMessage(key,
-                locale, getClassLoader(), args);
+    // --------------------------------------------------------------------
+    //               p r o p e r t i e s
+    // --------------------------------------------------------------------
+
+    public void setLookupForFileResource(final boolean value) {
+        _lookupForFileResource = value;
     }
 
-    public String getMessage(final String key, final Locale locale,
-                             final Map<String, ? extends Object> args) {
-        return super.getMessage(key,
-                locale, getClassLoader(), args);
-    }
+    // --------------------------------------------------------------------
+    //               p u b l i c
+    // --------------------------------------------------------------------
 
     public String getMessage(final String key, final String slocale,
                              final Object... args) {
@@ -49,6 +57,46 @@ public class BaseDictionary extends AbstractI18nBundle {
                              final Map<String, ? extends Object> args) {
         final Locale locale = LocaleUtils.getLocaleFromString(slocale);
         return this.getMessage(key, locale, args);
+    }
+
+    public String getMessage(final String key, final Locale locale,
+                             final Object... args) {
+        return this.validate(
+                super.getMessage(key, locale, getClassLoader(), args));
+    }
+
+    public String getMessage(final String key, final Locale locale,
+                             final Map<String, ? extends Object> args) {
+        return this.validate(
+                super.getMessage(key, locale, getClassLoader(), args));
+    }
+
+
+
+    // --------------------------------------------------------------------
+    //               p r i v a t e
+    // --------------------------------------------------------------------
+
+    private String validate(final String value) {
+        // should check if value is a file resource?
+        if (_lookupForFileResource) {
+            if (StringUtils.hasText(PathUtils.getFilenameExtension(value))) {
+                try {
+                    return this.readFile(value);
+                } catch (Throwable ignored) {
+                }
+            }
+        }
+
+        return value;
+    }
+
+    private String readFile(final String fileName) throws Exception{
+        final String result = ClassLoaderUtils.getResourceAsString(null, this.getClass(), fileName);
+        if(null==result){
+            throw new Exception("not a file");
+        }
+        return result;
     }
 
     // ------------------------------------------------------------------------
