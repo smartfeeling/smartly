4c29144dd3661a91887e77b0fbebd15158b73f60
Gian Angelo Geminiani <angelo.geminiani@gmail.com>
2012-11-13 13:26:21 +0100

    Added new methods from data conversion

--- smartly_package_01_velocity/src/org/smartly/packages/velocity/impl/vtools/ConvertTool.java
+++ smartly_package_01_velocity/src/org/smartly/packages/velocity/impl/vtools/ConvertTool.java
@@ -1,13 +1,6 @@
 package org.smartly.packages.velocity.impl.vtools;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.smartly.IConstants;
 import org.smartly.commons.util.ConversionUtils;
-import org.smartly.commons.util.JsonWrapper;
-
-import java.util.Collection;
-import java.util.Map;
 
 /**
  * Conversion utility
@@ -24,11 +17,62 @@ public class ConvertTool {
     }
 
     public String toString(final Object item) {
-        return null!=item ? item.toString():"";
+        return null != item ? item.toString() : "";
     }
 
-    public boolean toBoolean(final Object item){
+    public boolean toBoolean(final Object item) {
         return ConversionUtils.toBoolean(item);
+    }
+
+    public int toInt() {
+        return this.toInt(-1);
+    }
+
+    public int toInt(final Object value) {
+        return this.toInt(value, -1);
+    }
+
+    public int toInt(final Object value, final int defaultValue) {
+        try {
+            final Number n = ConversionUtils.toNumber(value);
+            return n.intValue();
+        } catch (Throwable ignored) {
+            return defaultValue;
+        }
+    }
+
+    public double toDouble() {
+        return this.toDouble(-1.0);
+    }
+
+    public double toDouble(final Object value) {
+        return this.toDouble(value, -1.0);
+    }
+
+    public double toDouble(final Object value, final double defaultValue) {
+        try {
+            final Number n = ConversionUtils.toNumber(value);
+            return n.doubleValue();
+        } catch (Throwable ignored) {
+            return defaultValue;
+        }
+    }
+
+    public long toLong() {
+        return this.toLong(-1);
+    }
+
+    public long toLong(final Object value) {
+        return this.toLong(value, -1);
+    }
+
+    public long toLong(final Object value, final int defaultValue) {
+        try {
+            final Number n = ConversionUtils.toNumber(value);
+            return n.longValue();
+        } catch (Throwable ignored) {
+            return defaultValue;
+        }
     }
 
     // ------------------------------------------------------------------------
