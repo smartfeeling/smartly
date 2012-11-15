33c34000151d77d880b9a1d3df67dcb1646b440e
Gian Angelo Geminiani <angelo.geminiani@gmail.com>
2012-11-15 12:50:49 +0100

    better compare between objects

--- smartly_core/src/org/smartly/commons/util/CompareUtils.java
+++ smartly_core/src/org/smartly/commons/util/CompareUtils.java
@@ -12,16 +12,14 @@ public class CompareUtils {
      */
     public static int compare(final Object o1, final Object o2) {
         if (null != o1 && null != o2) {
-            if (o1 instanceof Long) {
-                return ((Long) o1).compareTo((Long) o2);
-            } else if (o1 instanceof Integer) {
-                return ((Integer) o1).compareTo((Integer) o2);
-            } else if (o1 instanceof Byte) {
-                return ((Byte) o1).compareTo((Byte) o2);
-            } else if (o1 instanceof Double) {
-                return ((Double) o1).compareTo((Double) o2);
-            } else if (o1 instanceof String) {
-                return ((String) o1).compareTo((String) o2);
+            if (o1.getClass().equals(o2.getClass())) {
+                return compareSameTypes(o1, o2);
+            } else {
+                if (o1 instanceof Number && o2 instanceof Number) {
+                    return compareSameTypes(((Number)o1).doubleValue(), ((Number)o2).doubleValue());
+                } else {
+                    return compareSameTypes(o1.toString(), o2.toString());
+                }
             }
         } else if (null == o1) {
             return 1;
@@ -38,12 +36,35 @@ public class CompareUtils {
         return compare(o1, o2) == 0;
     }
 
+    public static boolean equals(final Number o1, final Number o2) {
+        return compare(o1, o2) == 0;
+    }
+
     public static boolean greater(final Object o1, final Object o2) {
         return compare(o1, o2) > 0;
     }
 
     public static boolean lower(final Object o1, final Object o2) {
         return compare(o1, o2) < 0;
+    }
+
+    // --------------------------------------------------------------------
+    //               p r i v a t e
+    // --------------------------------------------------------------------
+
+    private static int compareSameTypes(final Object o1, final Object o2) {
+        if (o1 instanceof Long) {
+            return ((Long) o1).compareTo((Long) o2);
+        } else if (o1 instanceof Integer) {
+            return ((Integer) o1).compareTo((Integer) o2);
+        } else if (o1 instanceof Byte) {
+            return ((Byte) o1).compareTo((Byte) o2);
+        } else if (o1 instanceof Double) {
+            return ((Double) o1).compareTo((Double) o2);
+        } else if (o1 instanceof String) {
+            return ((String) o1).compareTo((String) o2);
+        }
+        return -1;
     }
 
 }
---
+++ smartly_core/test/org/smartly/commons/util/CompareUtilsTest.java
@@ -0,0 +1,45 @@
+package org.smartly.commons.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * User: angelo.geminiani
+ */
+public class CompareUtilsTest {
+
+    public CompareUtilsTest() {
+
+    }
+
+    // ------------------------------------------------------------------------
+    //                      p r i v a t e
+    // ------------------------------------------------------------------------
+
+    @Test
+    public void testCompare() throws Exception {
+        int i = 10;
+        double d = 10.0;
+        long l = 10;
+        String s="10";
+
+        int result = CompareUtils.compare(i, d);
+        assertEquals(result, 0);
+
+        result = CompareUtils.compare(i, l);
+        assertEquals(result, 0);
+
+        result = CompareUtils.compare(i, s);
+        assertEquals(result, 0);
+
+        s="11";
+        result = CompareUtils.compare(i, s);
+        assertEquals(result, -1);
+
+        l=999999999;
+        d=999999999.000;
+        result = CompareUtils.compare(d,l);
+        assertEquals(result, 0);
+    }
+}
