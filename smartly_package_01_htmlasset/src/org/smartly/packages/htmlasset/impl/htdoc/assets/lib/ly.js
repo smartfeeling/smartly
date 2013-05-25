529d2f899da274e17f9ee7a80d906501c95e40b3
gian angelo geminiani <angelogeminiani@new-host-2.station>
2013-05-25 12:33:15 +0200

    update

--- smartly_package_01_htmlasset/src/org/smartly/packages/htmlasset/impl/htdoc/assets/lib/ly.js
+++ smartly_package_01_htmlasset/src/org/smartly/packages/htmlasset/impl/htdoc/assets/lib/ly.js
@@ -274,6 +274,28 @@
     }
 
     // ------------------------------------------------------------------------
+    //                      Utils Mobile
+    // ------------------------------------------------------------------------
+
+    var mobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i) ? true : false;
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i) ? true : false;
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i) ? true : false;
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
+        }
+    };
+
+    // ------------------------------------------------------------------------
     //                      Utils Objects
     // ------------------------------------------------------------------------
 
@@ -1086,6 +1108,9 @@
     exports.hasText = hasText;
     exports.replaceAll = replaceAll;
     exports.toBoolean = toBoolean;
+
+    //-- mobile --//
+    exports.mobile = mobile;
 
     //-- object utils --//
     exports.provide = provide;
