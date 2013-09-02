1d17fee1a93bcea21c412daf0b3c2c6590e433a1
gian angelo geminiani <angelogeminiani@MacBook-Pro-di-gian.local>
2013-09-02 10:21:55 +0200

    no message

--- smartly_package_02_http/src/org/smartly/packages/http/impl/util/resource/MemoryResource.java
+++ smartly_package_02_http/src/org/smartly/packages/http/impl/util/resource/MemoryResource.java
@@ -3,14 +3,17 @@ package org.smartly.packages.http.impl.util.resource;
 
 import org.eclipse.jetty.util.resource.Resource;
 import org.smartly.commons.util.DateUtils;
-import org.smartly.commons.util.FileUtils;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.ReadableByteChannel;
 
-public class MemoryResource extends Resource {
+public class MemoryResource
+        extends Resource {
 
     private final String _name;
     private final byte[] _content;
@@ -26,7 +29,7 @@ public class MemoryResource extends Resource {
     }
 
     @Override
-    public void release() {
+    public void close() {
 
     }
 
