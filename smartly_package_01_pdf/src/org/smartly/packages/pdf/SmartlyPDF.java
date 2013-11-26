e7450f56b9f96c5fc57fd5bbf4013e5afc822d08
gian angelo geminiani <angelogeminiani@MacBook-Pro-di-gian.local>
2013-11-26 10:30:28 +0100

    SmartlyPDF Package

---
+++ smartly_package_01_pdf/src/org/smartly/packages/pdf/SmartlyPDF.java
@@ -0,0 +1,11 @@
+package org.smartly.packages.pdf;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: angelogeminiani
+ * Date: 26/11/13
+ * Time: 10:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class SmartlyPDF {
+}
